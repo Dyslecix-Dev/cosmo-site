9d15132036f56fda0fe35cffe5de3e1e98a44e37
Christian Demesa <dyslecixdev@gmail.com>
2026-04-26 23:30:07 -0700

    fix: format files

--- public/theme.js
+++ public/theme.js
@@ -1,9 +1,5 @@
 function applyTheme() {
-  document.documentElement.classList.toggle(
-    "dark",
-    localStorage.theme === "dark" ||
-      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
-  );
+  document.documentElement.classList.toggle("dark", localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches));
 }
 function applyMotion() {
   if (localStorage.motion === "paused") {
